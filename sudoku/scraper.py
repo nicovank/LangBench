784ced8616f21700301425d61b8865eae5bad1a9
Nicolas van Kempen <nvankemp@gmail.com>
2023-05-05 09:35:51 -0400

    Minor changes

--- sudoku/scraper.py
+++ sudoku/scraper.py
@@ -3,25 +3,26 @@
 import sys
 import urllib.request
 from bs4 import BeautifulSoup
-import time
+
 
 def pull(x):
-	html = urllib.request.urlopen("https://grid.websudoku.com/?level=4&set_id={}".format(x)).read().decode("utf-8")
-	soup = BeautifulSoup(html, features="html.parser")
-	solution = soup.find(id="cheat")["value"]
-	mask = soup.find(id="editmask")["value"]
-	assert(len(solution) == 9 * 9)
-	assert(len(mask) == 9 * 9)
-	line = ""
-	for i in range(9 * 9):
-		z = int(mask[i])
-		line += str(((z ^ 1) & 1) * int(solution[i]))
-	print(line)
+    html = (
+        urllib.request.urlopen(f"https://grid.websudoku.com/?level=4&set_id={x}")
+        .read()
+        .decode("utf-8")
+    )
+    soup = BeautifulSoup(html, features="html.parser")
+    solution = soup.find(id="cheat")["value"]
+    mask = soup.find(id="editmask")["value"]
+    assert len(solution) == 9 * 9
+    assert len(mask) == 9 * 9
+    line = ""
+    for i in range(9 * 9):
+        z = int(mask[i])
+        line += str(((z ^ 1) & 1) * int(solution[i]))
+    print(line.replace("0", "."))
 
-def main(args):
-	for i in range(128):
-		pull(i)
-		time.sleep(30)
 
-if __name__ == '__main__':
-	main(sys.argv[1:])
+if __name__ == "__main__":
+    for i in range(128):
+        pull(i)
